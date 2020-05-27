9e2e0ddce9022b67b8298a08ed95831a28da527f
kayode <samsonkayode@gmail.com>
2020-05-27 16:39:41 +0100

    LRS UI Base Config

---
+++ LanRegistrySystemUI/src/main/java/com/lrs/ui/LanRegistrySystemUiApplication.java
@@ -0,0 +1,13 @@
+package com.lrs.ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanRegistrySystemUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LanRegistrySystemUiApplication.class, args);
+	}
+
+}
